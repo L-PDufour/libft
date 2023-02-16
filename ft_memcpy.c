ad59d7c9820a22770bb2f5b3579c122d073058ef
ldufour <ldufour@studen.42quebec.com>
2023-02-16 15:27:01 -0500

    strchr

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,27 +6,42 @@
 /*   By: ldufour <ldufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:22:44 by ldufour           #+#    #+#             */
-/*   Updated: 2023/02/16 14:28:58 by ldufour          ###   ########.fr       */
+/*   Updated: 2023/02/16 15:19:05 by ldufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    return(dst);
+	size_t		i;
+	const char	*s;
+	char		*d;
+
+	s = (char *)src;
+	d = (char *)dst;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
 
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-   const char src[50] = "http://www.tutorialspoint.com";
-   char dest[50];
-   strcpy(dest,"Heloooo!!");
-   printf("Before memcpy dest = %s\n", dest);
-   ft_memcpy(dest, src, strlen(src)+1);
-   printf("After memcpy dest = %s\n", dest);
-   
-   return(0);
+int	main(void)
+{
+	char src[50] = "http://www.tutorialspoint.com";
+	char dest[50];
+	strcpy(dest, "Heloooo!!");
+	printf("Before memcpy dest = %s\n", dest);
+	ft_memcpy(dest, src, strlen(dest) + 1);
+	printf("After memcpy dest = %s\n", dest);
+
+	return (0);
 }
---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldufour <ldufour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 15:08:05 by ldufour           #+#    #+#             */
+/*   Updated: 2023/02/16 15:19:34 by ldufour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	const   char	*s;
+	char	*d;
+
+	s = src;
+	d = dst;
+	i = 0;
+	while (i < len)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (d);
+}
+
+// int	main(void)
+// {
+// 	char dest[] = "oldstring";
+// 	const char src[] = "newstring";
+
+// 	printf("Before memmove dest = %s, src = %s\n", dest, src);
+// 	ft_memmove(dest, src, 9);
+// 	printf("After memmove dest = %s, src = %s\n", dest, src);
+
+// 	return (0);
+// }
