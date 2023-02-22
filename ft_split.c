4f40b9cb8487a6f163cb3597c253c83ded3c22d1
ldufour <ldufour@studen.42quebec.com>
2023-02-22 15:04:16 -0500

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldufour <ldufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/17 16:03:44 by leon              #+#    #+#             */
-/*   Updated: 2023/02/22 15:02:30 by ldufour          ###   ########.fr       */
+/*   Created: 2023/02/22 14:41:13 by ldufour           #+#    #+#             */
+/*   Updated: 2023/02/22 14:41:14 by ldufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	len;
-
-	if (!s1)
-		return (NULL);
-	if (!set)
-		return (ft_strdup(s1));
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
-	len = ft_strlen(s1);
-	while (len && ft_strchr(set, s1[len]))
-		len--;
-	return (ft_substr(s1, 0, len + 1));
-}
