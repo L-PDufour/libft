2312bf15a84443df25383342459cfd504437e697
lpdbond15 <leonpierre.dufour@gmail.com>
2023-02-21 20:03:49 -0500

    ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,18 +6,32 @@
 /*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:03:44 by leon              #+#    #+#             */
-/*   Updated: 2023/02/19 10:15:24 by leon             ###   ########.fr       */
+/*   Updated: 2023/02/21 17:53:59 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-		if (!(s1 = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
+	while ()
+	
+	char *trim;
 
-	return(s1);
+	return(trim);
+}
+
+int main ()
+{
+	char * s = ft_strtrim("   xxxtripouille", " x");
+	s = ft_strtrim("tripouille   xxx", " x");
+	s = ft_strtrim("   xxxtripouille   xxx", " x");
+	s = ft_strtrim("   xxx   xxx", " x");
+	s = ft_strtrim("", "123");
+	s = ft_strtrim("123", "");
+	s = ft_strtrim("", "");					
+	s = ft_strtrim("", "");
+	s = ft_strtrim("abcdba", "acb");
 }
