f0f916efa9a9b3324aadef0cf2afb36ba87a1b5f
lpdbond15 <leonpierre.dufour@gmail.com>
2023-02-24 11:28:34 -0500

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,50 @@
 /*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 14:41:13 by ldufour           #+#    #+#             */
-/*   Updated: 2023/02/22 20:34:57 by leon             ###   ########.fr       */
+/*   Updated: 2023/02/24 11:24:39 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+
+
+int ft_countWords(char const *s, char c)
+{
+    int words;
+    int i;
+
+    words = 0;
+    i = 0;
+    while (s[i]) 
+    {
+    // if (((i == 0) && s[i] != c) || (s[i - 1] == c && s[i] != c))  
+    if (s[i] != c)
+        words++;
+    i++;
+    }
+
+return (words);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	if (ptr == 0)
+		return (ptr);
+	while (i < nmemb * size)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	return (ptr);
+}
 
 char **ft_split(char const *s, char c)
 {
@@ -22,8 +60,9 @@ char **ft_split(char const *s, char c)
 
     if (!s)
         return NULL;
+    
     // Allocate memory for the array
-    strArray = malloc(ft_countWords(s, 'c') + 1);
+    strArray = ft_calloc(sizeof(char *), ft_countWords(s, c));
 
     while (*s) // Iterate through each character of the string
     {
@@ -36,11 +75,20 @@ char **ft_split(char const *s, char c)
             ++len;
         }
         // Allocate memory for individual string
-        strArray[i] = ft_strsub(s, 0, len);
+        strArray[i] = ft_calloc(sizeof(char), len);
         while (*s && *s == c) // Increment s until it points to a non-spacing character.
             ++s;
         ++i; // Increment index
     } 
-    strArray[i] = NULL; // Set last value as null
+    // strArray[i] = NULL; // Set last value as null
     return strArray; // Return the array of strings  
  }
+
+int main ()
+{
+    char *string = "1///////////////////2///3///4///5///////";
+    int words = ft_countWords(string, '/');
+    char  **word = ft_split(string, '/');
+    printf("%s", word[1]);
+    return (0);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldufour <ldufour@student.42.fr>            +#+  +:+       +#+        */
+/*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:28:03 by leon              #+#    #+#             */
-/*   Updated: 2023/02/23 16:23:54 by ldufour          ###   ########.fr       */
+/*   Updated: 2023/02/24 08:57:27 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,68 +15,35 @@
 #include "ft_calloc.c"
 #include "ft_strlcpy.c"
 
-// static char *vide(void)
-// {
-// 	char *out;
-
-// 	out = malloc(sizeof(char) * 1);
-// 	out[0] = '\0';
-// 	return (out);
-// }
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*dest;
-	size_t i;
-	size_t slen;
 
-	slen = len;
 	dest = ft_calloc(sizeof(char), (len) + 1);
 	if (!dest)
 		return (NULL);
-	i = 0;
- 	if (!s)
+    if (!s)
 		return (NULL);
-	if (len > ft_strlen(s - start))
-		len = ft_strlen(s - start);
-	while (i < len && s[start])
-    	{
-        dest[i] = s[start];
-        i++;
-        start++;
-    	}
+    if (ft_strlen(s) < start)
+		len = 0;
+    if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+    ft_strlcpy(dest, s + start, len + 1);
 	return (dest);
-
-	
-	// size_t  i;
-    // char    *str;
-    // size_t  s_len;
-    // size_t  str_len;
-    // i = 0;
-    // s_len = ft_strlen(s);
-    // if (s_len < start || len <= 0)
-    //     return (ft_calloc(1, sizeof(char)));
-    // if (len < s_len - start)
-    //     str_len = len;
-    // else
-    //     str_len = s_len - start;
-    // str = ft_calloc(str_len + 1, sizeof(char));
-    // if (!str)
-    //     return (NULL);
-    // while (i < str_len && s[start])
-    // {
-    //     str[i] = s[start];
-    //     i++;
-    //     start++;
-    // }
-    // str[i] = '\0';
-    // return (str);
 }
 
 // #include <stdio.h>
 // int main()
 // {
 // 	char *test;
-// 	test = ft_substr("lorem ipsum dolor sit amet", 7, 10);
-// 	printf("%s", test);
+// 	test = ft_substr("lorem ipsum dolor sit amet", 0, 10);
+// 	printf("%s\n", test);
+//     test = ft_substr("lorem ipsum dolor sit amet", 7, 10);
+// 	printf("%s\n", test);
+//     test = ft_substr("lorem ipsum dolor sit amet", 7, 0);
+// 	printf("%s\n", test);
+//     test = ft_substr("lorem ipsum dolor sit amet", 0, 0);
+// 	printf("%s\n", test);
+//     test = ft_substr("lorem ipsum dolor sit amet", 400, 20);
+// 	printf("%s\n", test);
 // }
